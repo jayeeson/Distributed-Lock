f187ecb0a00c832eedc1caed5e978b1bbcfa4bc8
Jayeeson <randomness363@hotmail.com>
2021-02-18 11:20:24 -0500

    parse cli using Commander

--- src/helpers/port.ts
+++ src/helpers/port.ts
@@ -1,22 +1,17 @@
-import { cliArgs } from '../config';
+import { Command } from 'commander';
 
-const getPortIndex = () => {
-  return process.argv.findIndex(argument =>
-    argument.toLocaleUpperCase().includes(cliArgs.port)
-  );
-};
+const program = new Command();
+program.option('p, --port', 'specify launch port (overrides .env)');
+const options = program.opts();
 
 export function getPort(): number | undefined {
-  const portIndex = getPortIndex();
-  const portArg =
-    portIndex > -1
-      ? parseInt(process.argv[portIndex].substr(cliArgs.port.length), 10)
-      : undefined;
-  const portEnv = parseInt(`${process.env.PORT}`, 10);
-
-  if (portArg && !isNaN(portArg) && portArg > 0) {
+  const portArg = parseInt(`${options.port}`, 10);
+  if (!isNaN(portArg) && portArg > 0) {
     return portArg;
-  } else if (!isNaN(portEnv) && portEnv > 0) {
+  }
+
+  const portEnv = parseInt(`${process.env.PORT}`, 10);
+  if (!isNaN(portEnv) && portEnv > 0) {
     return portEnv;
   }
   return undefined;
